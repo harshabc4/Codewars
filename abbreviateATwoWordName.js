f35a2d6962c73c3d6820f71f779659394f4d9005
Brian Harsha <harshabc4@gmail.com>
2022-03-16 16:30:09 -0400

    First codewars

---
+++ abbreviateATwoWordName.js
@@ -0,0 +1,8 @@
+function abbrevName(name){
+    let fullName=name.split(' ')
+    let firstInitial=fullName.shift().charAt(0)
+    let lastInitial=fullName.pop().charAt(0)
+    console.log($[firstInitial].$[lastInitial])
+  
+    
+    }
