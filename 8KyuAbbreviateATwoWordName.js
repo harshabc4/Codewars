4ae9d0adf4e330857cd95ef6cb573c2696a0a580
Brian Harsha <harshabc4@gmail.com>
2022-03-16 16:50:24 -0400

    cleaned up

--- 8KyuAbbreviateATwoWordName.js
+++ 8KyuAbbreviateATwoWordName.js
@@ -2,7 +2,12 @@ function abbrevName(name){
     let fullName=name.split(' ')
     let firstInitial=fullName.shift().charAt(0)
     let lastInitial=fullName.pop().charAt(0)
-    console.log($[firstInitial].$[lastInitial])
-  
-    
+    return(`${firstInitial}.${lastInitial}`).toUpperCase();
     }
+
+// Top Rated
+function abbrevName(name){
+
+    var nameArray = name.split(" ");
+    return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+  }
