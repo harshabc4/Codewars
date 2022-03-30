6e11b3986572f96843a401bcfe56c304dcd5d796
Brian Harsha <harshabc4@gmail.com>
2022-03-30 19:39:50 -0400

    daily codewars 16 kata 5

---
+++ 8KyuBeginnerLostWithoutAMap.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+// Mine
+const maps = x => x.map(x => x * 2)
+
+// Top Rated
+function maps(x){
+    return x.map(n => n * 2);
+  }
