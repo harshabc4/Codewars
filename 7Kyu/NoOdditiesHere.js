be68a6b199176f0b40e5ec1858dc08a91df1f311
Brian Harsha <harshabc4@gmail.com>
2022-04-23 05:58:14 -0400

    daily codewars 40

---
+++ 7Kyu/NoOdditiesHere.js
@@ -0,0 +1,17 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+// Mine
+function noOdds( values ){
+    return(values.filter(x => x % 2 == 0))
+  }
+
+// Top Rated
+function noOdds( values ){
+    function isEven(number){
+    return number%2 == 0;
+    }
+    
+    return values.filter(isEven);
+  }
