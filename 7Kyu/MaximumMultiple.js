cc4ba7a6afef053c01e9e7f873015a5334b28eb4
Brian Harsha <harshabc4@gmail.com>
2022-04-25 17:40:46 -0400

    daily codewars 41

---
+++ 7Kyu/MaximumMultiple.js
@@ -0,0 +1,57 @@
+// Task
+
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+// Conditions :
+
+//     N is divisible by divisor
+
+//     N is less than or equal to bound
+
+//     N is greater than 0.
+
+// Notes
+
+//     The parameters (divisor, bound) passed to the function are only positive values .
+//     It's guaranteed that a divisor is Found .
+//     Input >> Output Examples
+
+// maxMultiple (2,7) ==> return (6)
+
+// Explanation:
+
+// (6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+
+// maxMultiple (10,50)  ==> return (50)
+
+// Explanation:
+
+// (50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .*
+
+// maxMultiple (37,200) ==> return (185)
+
+// Explanation:
+
+// (185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+// Playing with Numbers Series
+// Playing With Lists/Arrays Series
+// Bizarre Sorting-katas
+// For More Enjoyable Katas
+// ALL translations are welcomed
+// Enjoy Learning !!
+// Zizou
+
+// Mine
+function maxMultiple(divisor, bound){
+    let answerArr = []
+    for(let i = 1; i <= bound; i++){
+      if (i%divisor==0){
+        answerArr.unshift(i)
+      }
+    }
+    return answerArr[0]
+  }
+
+// Top Rated
+function maxMultiple(divisor, bound){
+    return bound-bound%divisor
+  }
