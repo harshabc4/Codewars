5212bbc14f8ea3cfc3395a84fb885aa27b3110b8
Brian Harsha <harshabc4@gmail.com>
2022-04-30 13:37:29 -0400

    bonus codewar 7 of 7

---
+++ 7Kyu/SortArrays.js
@@ -0,0 +1,9 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// Mine
+const sortme = names => names.sort()
+
+// Top Rated
+sortme = function( names ){
+    return names.sort()
+  }
