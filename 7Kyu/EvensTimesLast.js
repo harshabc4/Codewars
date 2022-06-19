47ec2014369ea890d3e953819e046636bfb23f6a
Brian Harsha <harshabc4@gmail.com>
2022-06-19 08:29:16 -0400

    daily codewars 97

---
+++ 7Kyu/EvensTimesLast.js
@@ -0,0 +1,21 @@
+// Description:
+
+// Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
+
+// Indices in sequence start from 0.
+
+// If the sequence is empty, you should return 0.
+
+
+// Mine
+const evenLast = numbers => numbers.length >= 1 ? numbers.filter((el,i) => i%2===0).reduce((acc, c) => acc + c, 0)*numbers[numbers.length-1] : 0
+
+// Top Rated
+function evenLast(numbers) {
+    if (numbers.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i += 2) {
+      sum += numbers[i];
+    }
+    return sum * numbers[numbers.length - 1];
+  }
