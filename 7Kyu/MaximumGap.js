333f0a59ba5f2268a8ef281373f6fe1d29b3dd19
Brian Harsha <harshabc4@gmail.com>
2022-06-25 08:08:42 -0400

    daily codewars 103

---
+++ 7Kyu/MaximumGap.js
@@ -0,0 +1,79 @@
+// Description:
+// Introduction and Warm-up (Highly recommended)
+// Playing With Lists/Arrays Series
+// Task
+
+// Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+// Notes
+
+//     Array/list size is at least 3 .
+
+//     Array/list's numbers Will be mixture of positives and negatives also zeros_
+
+//     Repetition of numbers in the array/list could occur.
+
+//     The Maximum Gap is computed Regardless the sign.
+
+// Input >> Output Examples
+
+// maxGap ({13,10,5,2,9}) ==> return (4)
+
+// Explanation:
+
+//     The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+
+// maxGap ({-3,-27,-4,-2}) ==> return (23)
+
+// Explanation:
+
+//     The Maximum Gap after sorting the array is 23 , The difference between |-4- (-27) | = 23 .
+
+//     Note : Regardless the sign of negativity .
+
+// maxGap ({-7,-42,-809,-14,-12}) ==> return (767)  
+
+// Explanation:
+
+//     The Maximum Gap after sorting the array is 767 , The difference between | -809- (-42) | = 767 .
+
+//     Note : Regardless the sign of negativity .
+
+// maxGap ({-54,37,0,64,640,0,-15}) //return (576)
+
+// Explanation:
+
+//     The Maximum Gap after sorting the array is 576 , The difference between | 64 - 640 | = 576 .
+
+//     Note : Regardless the sign of negativity .
+
+// Playing with Numbers Series
+// Playing With Lists/Arrays Series
+// For More Enjoyable Katas
+// ALL translations are welcomed
+// Enjoy Learning !!
+// Zizou
+// Fundament
+
+// Mine
+function maxGap (numbers){
+    let highestDiff = 0
+    let sortedArr = numbers.sort((a,b)=>a-b)
+    for(let i = 1; i < sortedArr.length; i++){
+      if(sortedArr[i] - sortedArr[i-1] > highestDiff){
+        highestDiff = (sortedArr[i] - sortedArr[i-1])
+      }
+    }
+    return highestDiff
+  }
+
+// Top Rated
+function maxGap (numbers){
+    let num = numbers.sort((a,b) => a - b)
+    let sum = num[1] - num[0]
+    for(i=0; i < num.length; i++){
+       if(num[i+1] - num[i] > sum){
+         sum = num[i+1] - num[i]
+       }
+     }
+    return sum
+   }
