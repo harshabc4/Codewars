5e3766984350a11d471c53ff61951d7809ea0cd5
Brian Harsha <harshabc4@gmail.com>
2022-07-01 06:53:22 -0400

    daily codewars 109

---
+++ 7Kyu/ForTheSakeOfArgument.js
@@ -0,0 +1,21 @@
+// Description:
+
+// Write a function named numbers.
+
+// function should return True if all parameters are of the Number type.
+
+// The function should accept any number of parameters.
+
+// Example usage:
+
+// numbers(1, 4, 3, 2, 5); // true
+// numbers(1, "a", 3); // false
+// numbers(1, 3, NaN); // true
+
+
+// Create your numbers function here
+function numbers() {
+    return [].every.call(arguments, function(value) {
+      return typeof value === "number";
+    });
+  }
