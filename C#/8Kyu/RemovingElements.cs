69f3ed206efb4e885c06b1fc1881a9724003bd00
Brian Harsha <harshabc4@gmail.com>
2022-09-08 19:39:56 -0400

    codewar 22

---
+++ C#/8Kyu/RemovingElements.cs
@@ -0,0 +1,18 @@
+// Description:
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+using System.Linq;
+
+public static class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+        return arr.Where((e, i) => i%2 == 0).ToArray();
+    }
+}
